5bd5dd375cb2a43267edbca6bcb912f764349725
Stark seven <125395357+kaiqueMNM@users.noreply.github.com>
2023-07-13 17:44:23 -0300

    sipec

---
+++ JS/select.js
@@ -0,0 +1,38 @@
+let displayedError = '';
+
+function showErrorText(elementId) {
+  const errorText = document.getElementById(elementId);
+  if (displayedError !== elementId) {
+    errorText.style.display = 'block';
+    if (displayedError !== '') {
+      const previousError = document.getElementById(displayedError);
+      previousError.style.display = 'none';
+    }
+    displayedError = elementId;
+  }
+}
+
+function limparTexto() {
+  const errorText = document.getElementById(displayedError);
+  if (errorText) {
+    errorText.style.display = 'none';
+    displayedError = '';
+  }
+}
+//mudouuuu//
+document.addEventListener('DOMContentLoaded', function() {
+  var isLoggedIn = false; // Variável que controla o status do login
+
+  // Função para verificar se o usuário está logado
+  function checkLoginStatus() {
+    if (!isLoggedIn) {
+      // Redirecionar para a página de login
+      window.location.href = "file:///S:/SUPERVISORES/05%20DANRLEI%20SOUZA/Base%20de%20Dados%20SIPEC-DECIPEX/index.html";
+    }
+  }
+
+  // Verificar o status do login ao carregar a página
+  checkLoginStatus();
+});
+
+//mudouuuu//
